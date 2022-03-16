b417ff16062102217ba1e7fbe02ecb05acf03f1f
michael <87787413+cello-phane@users.noreply.github.com>
2022-03-15 19:02:14 -0700

    Update padder.py

--- padder.py
+++ padder.py
@@ -23,9 +23,11 @@ def padder(header = "begin", footer = "end", filler = "-", count = 2):
         header_ = pads + header + pads
         footer_ = pads + footer + pads
     len_header_ = len(header_)
-    if len_header > len(footer_):
+    if len_header != len(footer_):
         if len(footer_) % 2 == 0:
             footer_ = footer_ + filler * (abs(len(header_) - len(footer_)))
+        if len(header_) % 2 == 0:
+            header_ = header_ + filler * (abs(len(header_) - len(footer_)))
     if len(header_) > len(footer_):
         footer_ += filler
     if len(footer_) > len(header_):
