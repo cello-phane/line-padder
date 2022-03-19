a92872053eb01995fbe0d76b90d2b8f9b1766540
michael <87787413+cello-phane@users.noreply.github.com>
2022-03-19 01:32:51 -0700

    Update padder.py

--- padder.py
+++ padder.py
@@ -10,16 +10,12 @@ def padder(header = "begin", footer = "end", filler = "-", count = 0):
     padded_header_size = len_header + len_pads
     padded_footer_size = len_footer + len_pads
     offset = int(abs(len_header - len_footer) / 2)
-    fillc = 0
-    if padded_header_size >= padded_footer_size:
-        if padded_header_size != (fillc * 2) + len_header:
-            fillc = count - offset
-        header_ = (filler * (count - offset)) + header + (filler * fillc)
+    fillc = abs(count - offset)
+    if padded_header_size > padded_footer_size:    
+        header_ = filler * fillc + header + filler * fillc
         footer_ = pads + footer + pads
-    elif padded_footer_size >= padded_header_size:
-        if padded_footer_size != (fillc * 2) + len_footer:
-            fillc = count - offset
-        footer_ = (filler * (count - offset)) + footer + (filler * fillc)
+    elif padded_footer_size > padded_header_size:
+        footer_ = filler * fillc + footer + filler * fillc
         header_ = pads + header + pads
     else:
         header_ = pads + header + pads
@@ -29,14 +25,10 @@ def padder(header = "begin", footer = "end", filler = "-", count = 0):
             footer_ = footer_ + filler * (abs(len(header_) - len(footer_)))
         elif len(footer_) > len(header_):
             header_ = header_ + filler * (abs(len(footer_) - len(header_)))
-        if len(footer_) % 2 != 0 and len(header_) % 2 == 0:
-            footer_ += filler
-        if len(footer_) % 2 == 0 and len(header_) % 2 != 0:
-            header_ += filler
     return header_, footer_
-header = 'header'
-footer = 'footer'
+header = 'Header'
+footer = 'Footer'
 filler = '-'
-head, foot = padder(header = header, footer = footer, filler = filler, count = 9)
+head, foot = padder(header = header, footer = footer, filler = filler)
 print(head)
 print(foot)
