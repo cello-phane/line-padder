34f0cd9e5771c89877c09478dfcb49b41e32d641
michael <87787413+cello-phane@users.noreply.github.com>
2022-03-26 19:09:54 -0700

    Update padder.py

--- padder.py
+++ padder.py
@@ -10,12 +10,16 @@ def padder(header = "begin", footer = "end", filler = "-", count = 0):
     padded_header_size = len_header + len_pads
     padded_footer_size = len_footer + len_pads
     offset = int(abs(len_header - len_footer) / 2)
-    fillc = abs(count - offset)
-    if padded_header_size > padded_footer_size:    
-        header_ = filler * fillc + header + filler * fillc
+    fillc = 0
+    if padded_header_size >= padded_footer_size:
+        if padded_header_size != (fillc * 2) + len_header:
+            fillc = count - offset
+        header_ = (filler * (count - offset)) + header + (filler * fillc)
         footer_ = pads + footer + pads
-    elif padded_footer_size > padded_header_size:
-        footer_ = filler * fillc + footer + filler * fillc
+    elif padded_footer_size >= padded_header_size:
+        if padded_footer_size != (fillc * 2) + len_footer:
+            fillc = count - offset
+        footer_ = (filler * (count - offset)) + footer + (filler * fillc)
         header_ = pads + header + pads
     else:
         header_ = pads + header + pads
@@ -25,10 +29,56 @@ def padder(header = "begin", footer = "end", filler = "-", count = 0):
             footer_ = footer_ + filler * (abs(len(header_) - len(footer_)))
         elif len(footer_) > len(header_):
             header_ = header_ + filler * (abs(len(footer_) - len(header_)))
+        if len(footer_) % 2 != 0 and len(header_) % 2 == 0:
+            footer_ += filler
+        if len(footer_) % 2 == 0 and len(header_) % 2 != 0:
+            header_ += filler
     return header_, footer_
-header = 'Header'
-footer = 'Footer'
+header = 'columne'
+footer = 'this footer'
 filler = '-'
-head, foot = padder(header = header, footer = footer, filler = filler)
-print(head)
-print(foot)
+corner = '+'
+line = 'data data data'
+width = int(len(line)/2)+1
+head, foot = padder(header = header, footer = '', filler = filler, count = width)
+fill_ws = int(abs(len(head) - len(line))/2) or 1
+if len(head) % 2 == 0:
+    if len(head) > (fill_ws*2) + len(line):
+        fill_ws += 1
+        if not header:
+            right_ = filler + corner
+        else:
+            right_ = corner
+    else:
+        if not header and len(line) % 2 != 0:
+            right_ = filler + corner
+        else:
+            if len(line) % 2 != 0:
+                right_ = filler + corner
+            else:
+                right_ = corner
+    line = ' ' * (fill_ws) + line + ' ' * (fill_ws)
+    left_ = corner
+else:
+    left_ = corner
+    right_ = corner
+    if len(line) % 2 == 0:
+        if len(line) > len(header):
+            head = head[:-1]
+            foot = foot[:-1]
+        else:
+            line += ' '
+
+    line = ' ' * (fill_ws) + line + ' ' * (fill_ws)
+print(left_ + head + right_)
+print('|' + line + '|')
+if footer:
+    if int(width/2)-1 % 2 != 0:
+        fill_ = (int(width/2)-1)-1
+        left_ = corner + (filler * (fill_+1))
+    else:
+        fill_ = (int(width/2)-1)-1
+        left_ = corner + (filler * fill_)
+    foot = footer
+    right_ = (filler * fill_) + corner
+print(left_ + foot + right_)
