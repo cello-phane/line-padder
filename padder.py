ad3f9a55ace166c92684f0661c3f24ecb807176b
michael <87787413+cello-phane@users.noreply.github.com>
2022-09-11 01:21:10 -0700

    Create padder.py

--- padder.py
+++ padder.py
@@ -34,16 +34,18 @@ def padder(header = "begin", footer = "end", filler = "-", count = 0):
         if len(footer_) % 2 == 0 and len(header_) % 2 != 0:
             header_ += filler
     return header_, footer_
-def columnize(max_line_len, width, head, foot):
+def columnize(max_line_len, head, foot):
     fill_ws = int(abs(len(head) - max_line_len)/2) or 1
+    corner = '*'
+    filler = "-"
     if len(head) % 2 == 0:
         if len(head) > (fill_ws*2) + max_line_len:
-            if not header:
+            if not head:
                 hright_ = filler + corner
             else:
                 hright_ = corner
         else:
-            if not header and max_line_len % 2 != 0:
+            if not head and max_line_len % 2 != 0:
                 hright_ = filler + corner
             else:
                 if max_line_len % 2 != 0:
@@ -55,39 +57,30 @@ def columnize(max_line_len, width, head, foot):
         hleft_ = corner
         hright_ = corner
         if max_line_len % 2 == 0:
-            if max_line_len > len(header):
+            if max_line_len > len(head):
                 head = head[:-1]
                 foot = foot[:-1]
     fleft_ = hleft_
     fright_ = hright_
     #return formatted header and footer
     return fleft_, fright_, hright_, hleft_, head, foot
-header = 'The Shining Column Data'
-footer = 'This footer was typed by a ghost'
-filler = '-'
-corner = '*'
+def print_padded(strings_, header_, footer_):
+    maxlinelen = len(max(strings_, key = len))
+    minlinelen = len(min(strings_, key = len))
+    (head,foot) = padder(header = header_, footer = footer_, filler = "-", count = 0)
+    fleft_, fright_, hright_, hleft_, head, foot = columnize(maxlinelen, head, foot)
+    len_of = len(hleft_ + head + hright_)
+    expand_len_t = int((maxlinelen - len_of) // 2)+1
+    expand_len_b = int((maxlinelen - len_of) // 2)+1
+    print(hleft_ + (expand_len_t * '-') + head + (expand_len_t * '-') + hright_)
+    for l in strings_:
+        print(" " + l)
+    print(hleft_ + (expand_len_b * '-') + foot + (expand_len_b * '-') + hright_)      
 lines = ['all code and no compile makes jack nullboy',
 		'all code and no compile makesjack a null boy',
 		'all code and compile makes jack a null',
 		'all code and no compile makes jack a null boyyy',
-		'all code and no compile makes jack a null b',
+		'all code and no compile makes jack a null byy',
 		'all code and no compile makes jack a null bo']
-maxlinelen = len(max(lines, key = len))
-diff = int((maxlinelen - len(max(header, footer)))/2)
-if diff % 2 == 0:
-  width = int(maxlinelen/diff) + (divmod(diff, 2)[0] - divmod(diff, 2)[1]) 
-else:
-  width = int(maxlinelen/diff)+1 + divmod(diff, 2)[0]  + divmod(diff, 2)[1]
-head_, foot_ = padder(header = header, footer = footer, filler = filler, count = width+1)
-fleft, fright, hright, hleft, head, foot = columnize(max_line_len = maxlinelen, width=int(maxlinelen/2)+1, head = head_, foot = foot_)
 #print out column 1
-for idx, line in enumerate(lines):
-	if idx == 0:
-		print(hleft + head + hright)
-		print('| ' + line.ljust(maxlinelen,' ') + ' |')
-	elif idx == len(lines)-1:
-		print('| ' + line.ljust(maxlinelen,' ') +' |')
-		print(fleft + foot + fright)
-	else:
-		print('| ' + line.ljust(maxlinelen,' ') + ' |')
-#print(fleft + foot + fright)
+print_padded(lines, header_="begin", footer_="end")
