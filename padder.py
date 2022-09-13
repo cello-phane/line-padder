26e1a0549d8141feb2b8f3bdef2904ca11582980
michael <87787413+cello-phane@users.noreply.github.com>
2022-09-12 22:57:22 -0700

    Update padder.py

--- padder.py
+++ padder.py
@@ -1,8 +1,6 @@
-def padder(header = "", footer = "", filler = "-", count = 0):
-    if count == 0:
-        count = int((max(len(header), len(footer)) - min(len(header), len(footer))) /2) + 2
-    if not filler:
-        filler = ' '
+def padder(header = "", footer = "", filler = "", count = ""):
+    if not count or count == 0:
+        count = int((max(len(header), len(footer)) - min(len(header), len(footer))) // 2) + 2
     pads = filler * count
     len_pads = len(pads)
     len_header = len(header)
@@ -34,10 +32,8 @@ def padder(header = "", footer = "", filler = "-", count = 0):
         if len(footer_) % 2 == 0 and len(header_) % 2 != 0:
             header_ += filler
     return header_, footer_
-def columnize(max_line_len, head, foot):
+def columnize(max_line_len, head, foot, corner=''):
     fill_ws = int(abs(len(head) - max_line_len)/2) or 1
-    corner = '*'
-    filler = "-"
     if len(head) % 2 == 0:
         if len(head) > (fill_ws*2) + max_line_len:
             if not head:
@@ -65,25 +61,29 @@ def columnize(max_line_len, head, foot):
     #return formatted header and footer
     return fleft_, fright_, hright_, hleft_, head, foot
 def print_padded(strings_, header_, footer_):
-    (head,foot) = padder(header = header_, footer = footer_, filler = "-", count = 0)
+    filler_ = '-'
+    corner_ = '-'
+    sidel_ = '|'
+    sider_ = '|'
+    count_ = 0 #this 0 says to auto-resize the column
+    (head,foot) = padder(header = header_, footer = footer_, filler = filler_, count = count_)
     maxlinelen = len(max(strings_, key = len))
     if len(head) > maxlinelen:
-        maxlinelen = len(head) + 2
-    else:
-        maxlinelen += 4
+        maxlinelen = len(head)
     minlinelen = len(min(strings_, key = len))
-    fleft_, fright_, hright_, hleft_, head, foot = columnize(maxlinelen, head, foot)
+    fleft_, fright_, hright_, hleft_, head, foot = columnize(maxlinelen, head, foot, corner_)
     len_of = len(hleft_ + head + hright_)
-    expand_len_t = int((maxlinelen - len_of) // 2)
-    expand_len_b = int((maxlinelen - len_of) // 2)
-    print(hleft_ + (expand_len_t * '-') + head + (expand_len_t * '-') + hright_)
+    expand_len_t = int((maxlinelen - len_of) // 2) + 1
+    expand_len_b = int((maxlinelen - len_of) // 2) + 1
+    print(hleft_ + (expand_len_t * filler_) + head + (expand_len_t * filler_) + hright_)
     for l in strings_:
-        if len(l) < maxlinelen:
-            ws_ = " " * int((maxlinelen - len(l)) - 4)
-            print('| ' + l + ws_ + " |")
-        else:
-            print('| ' + l + ' |')
-    print(fleft_ + (expand_len_b * '-') + foot + (expand_len_b * '-') + fright_)
+        if l:
+            if len(l) < maxlinelen:
+                ws_ = " " * int((maxlinelen - len(l)))
+                print(sidel_ + l + ws_ + sider_)
+            else:
+                print(sidel_ + l + sider_)
+    print(fleft_ + (expand_len_b * filler_) + foot + (expand_len_b * filler_) + fright_)
 lines = ['This is sample text:',
         'Column text auto-resizes',
         'according to any cell\'s',
